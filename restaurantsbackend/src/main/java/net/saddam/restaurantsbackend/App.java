4df0ae836d7139f6fcc0175313808427edd19309
Sk Saddam Hosan <sksddmhosan@gmail.com>
2017-09-11 23:04:16 +0530

    My Project setup completed!!

---
+++ restaurantsbackend/src/main/java/net/saddam/restaurantsbackend/App.java
@@ -0,0 +1,13 @@
+package net.saddam.restaurantsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
